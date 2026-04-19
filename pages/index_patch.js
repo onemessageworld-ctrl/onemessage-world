a9de3ef3fdc317f6ec4cb6e3293898d9e6af41ba
onemessageworld-ctrl <onemessage.world@gmail.com>
2026-04-19 00:00:54 -0700

    Remove patch file

--- pages/index_patch.js
+++
@@ -1,8 +0,0 @@
-// This file patches index.jsx to use real Stripe
-// Run this once: node pages/index_patch.js
-const fs = require('fs');
-let c = fs.readFileSync('./pages/index.jsx','utf8');
-// Add loading state after paid state
-c = c.replace("const[paid,setPaid]=useState(false);", "const[paid,setPaid]=useState(false);\n  const[loading,setLoading]=useState(false);");
-fs.writeFileSync('./pages/index.jsx', c);
-console.log('Patched');
